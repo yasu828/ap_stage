c8420fc7201c22c879de96efb945b7d46cc8830c
yasu828 <69378134+yasu828@users.noreply.github.com>
2022-02-19 17:42:08 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,240 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="top-body">
+        <router-view></router-view>
+    <section class="images">
+      <!-- メモ：後ろの画像はback-groud -->
+      <div class="box imageanimationleft">
+        <p class="imagestextleft">テキストメッセージ</p>
+      </div>
+      <div class="box imageanimationcenter">
+        <p class="imagestextcenter">
+          テキストメッセージ<br>
+          テキストメッセージ
+        </p>
+      </div>
+      <div class="box imageanimationright">
+        <p class="imagestextright">
+          テキストメッセージ<br>
+          テキストメッセージ<br>
+          テキストメッセージ
+        </p>
+      </div>
+    </section>
+
+    <section class="anchor">
+      <span class="anchor-text pointer" @click="applyScroll">
+        プロフィール<br>
+        ⌵
+      </span>
+    </section>
+    <!-- <transition appear> -->
+      <section class="apply" id="apply">
+        <div class="box b-green">
+          <h2 class="t-center">
+            あなたは<br>
+            なぜ<br>
+            我が社に？<br>
+          </h2>
+          <div class="t-center w-max">
+            <router-link to="first">
+              <span class="t-center">リンク先</span>
+            </router-link>
+          </div>
+        </div>
+        <div class="box b-blue">
+          <h2 class="t-center">
+            あなたは<br>
+            どうなりたい？<br>
+          </h2>
+          <div class="t-center w-max">
+            <router-link to="first">
+              <span class="t-center">リンク先</span>
+            </router-link>
+          </div>
+        </div>
+      </section>
+    <!-- </transition> -->
+    <section class="disablity">
+      <router-link to="/first">
+      <span class="lin">別リンク</span>
+      </router-link>
+    </section>
+
+    <section class="other">
+      <div class="inner">
+        <h3>その他グループ</h3>
+        <div class="companies">
+          <h4>タイトル</h4>
+          <div class="toggleArea">
+            <p>
+              <strong>企業名<br></strong>
+              コメント-------------------------
+            </p>
+            <router-link
+            to="second"
+            class="sec"
+            >
+              <span class="blue-btn">企業サイトへ</span>
+            </router-link>
+          </div>
+        </div>
+        <div class="companies">
+          <h4>タイトル</h4>
+          <div class="toggleArea">
+            <p>
+              <strong>企業名<br></strong>
+              コメント-------------------------
+            </p>
+            <router-link
+            to="second"
+            class="sec"
+            >
+              <span class="blue-btn">企業サイトへ</span>
+            </router-link>
+          </div>
+        </div>
+        <div class="companies">
+          <h4>タイトル</h4>
+          <div class="toggleArea">
+            <p>
+              <strong>企業名<br></strong>
+              コメント-------------------------
+            </p>
+            <router-link
+            to="second"
+            class="sec"
+            >
+              <span class="blue-btn">企業サイトへ</span>
+            </router-link>
+          </div>
+        </div>
+        <div class="companies">
+          <h4>タイトル</h4>
+          <div class="toggleArea">
+            <p>
+              <strong>企業名<br></strong>
+              コメント-------------------------
+            </p>
+            <router-link
+            to="second"
+            class="sec"
+            >
+              <span class="blue-btn">企業サイトへ</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="footer">
+        フッター
+      </div>
+    </footer>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style scoped lang="scss">
+@import './sass/app.scss';
+@import './sass/default.scss';
+@import './sass/reset.scss';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+#top-body{
+  .t-center{
+    text-align: center;
   }
-}
-</script>
+  .w-max{
+    width: 100%;
+  }
+  .b-green{
+    background-color: rgba(172, 255, 47, 0.486);
+  }
+  .b-blue{
+    background-color: rgba(135, 135, 235, 0.603);
+  }
+  box-sizing: border-box;
+  width: 100%;
+  border: solid #5B6DCD 5px;
+  
+  
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .anchor{
+    height: 100px;
+    z-index: 2;
+    background-color: #000;
+    text-align: center;
+
+    .anchor-text{
+      text-decoration: none;
+      list-style: none;
+      font-size: 36px;
+      color: #fff;
+    }
+  }
+
+  .apply{
+    display: flex;
+    .box{
+      width: 50%;
+      height: 582px;
+      min-height: 40vw;
+      border:#4f1486 solid 1px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+  }
+
+  .disablity{
+    height: 170px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .other{
+    h3{
+      width: 100%;
+      padding: 62px 0px 31px 0px;
+      text-align: center;
+      display: block;
+    }
+    .inner{
+      display: flex;
+      flex-wrap: wrap;
+      max-width: 980px;
+      margin: 0 auto;
+      .companies{
+        border: solid 2px rgb(41, 41, 105);
+        margin: 0px 14px 21px 14px;
+        width: calc(50% - 36px);
+        text-align: center;
+
+        a{
+          display: block;
+          width: 45%;
+          height: auto;
+          margin: 0px auto;
+          text-decoration: none;
+          border-radius: 100px;
+          background: linear-gradient(to right, #22a6e6 30%, #004db1 100%);
+          padding: 10px;
+
+          .blue-btn{
+            color: white;
+          }
+        }
+        .toggleArea{
+          padding: 0px 0px 35px 0px;
+          background-color: #fff;
+        }
+      }
+    }
+  }
+  
+  .footer{
+    height: 150px;
+    background-color: rgba(156, 202, 156, 0.507);
+  }
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    messe: "じゃじゃj",
+    counter: 0,
+    arr:["o","2","jj"],
+  },
+  mutations: {
+    inc(state) {
+      state.masse = state.counter
+    },
+    incdes(state){
+      console.log("aa")
+      state.arr.push("ee")
+      state.counter = state.messe
+    }
+  },
+  getters: {
+    arr(state) {
+      return state.arr.length
+    }
+  },
+  actions: {
+    incdes(state){
+      console.log("aa")
+      state.counter++;
+      state.commit('incdes')
+    },
+    incs(state) {
+      
+      state.commit('incs')
+    }
+  },
+  modules: {
+  }
+})
