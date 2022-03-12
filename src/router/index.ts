dcbb7bc5ffa07209766a248be2569b7662b26a88
yasu828 <69378134+yasu828@users.noreply.github.com>
2022-03-12 20:20:12 +0900

    router整理

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,42 +8,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/contents',
-    name: 'Contents',
-    component: () => import('../views/TargetContents.vue')
-  },
-  {
     path: '/inter',
     name: 'Inter',
     component: () => import('../views/intersectionHome.vue')
-  },
-  {
-    path: '/intersection-right',
-    name: 'intersectionRight',
-    component: () => import('../components/intersection/intersectionRight.vue')
-  },
-  {
-    path: '/intersection-left',
-    name: 'intersectionLeft',
-    component: () => import('../components/intersection/intersectionLeft.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/first',
-    name: 'first',
-    component: () => import('../components/intersection/first.vue'),
-    children: [
-      {
-        // /user/:id/profile がマッチした時に
-        // UserProfile は User の <router-view> 内部で描画されます
-        path: 'first_child',
-        component: () => import('../components/ex/first_child.vue'),
-      },
-    ]
   },
 ]
 
